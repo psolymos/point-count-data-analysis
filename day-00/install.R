13b5a3bf887e05de898921a3d4854159fd5667be
Peter Solymos <psolymos@gmail.com>
2025-10-13 16:01:45 -0600

    Ask before install
    Signed-off-by: Peter Solymos <psolymos@gmail.com>

--- day-00/install.R
+++ day-00/install.R
@@ -1,32 +1,59 @@
-cat("Helo!\n\nThis script will install required R packages for the\nPoint Count Data Analysis Workshop.\n\nHang tight ...\n\n* Checking R version ... ")
-if (getRversion() < 4) {
-  stop(paste0("R version ", getRversion(), " detected.",
-    " Please upgrade to R >= 4.0"))
-}
-cat("OK\n* Installing R packages ... \n")
+dryrun <- FALSE
+ask <- TRUE
 
-pkgs <- c("bookdown", "remotes", "dismo",
-  "Distance", "forecast", "glmnet", "gbm", "intrval",
-  "knitr", "lme4", "maptools", "mefa4", "corrplot", "sf",
-  "mgcv", "MuMIn", "opticut", "partykit", "pscl", "raster",
-  "ResourceSelection", "shiny", "unmarked", "visreg")
+cat("Helo!\n\nThis script will install required R packages for the\nPoint Count Data Analysis Workshop.\n\n")
 
-gh_pkgs <- c(bSims="psolymos/bSims", # use dev
-             detect="psolymos/detect", # use dev
-             lhreg="psolymos/lhreg") # not on CRAN
+if (ask) {
+  cat("Do you want to proceed?")
+  if (utils::menu(c("Yes", "No")) != 1L){
 
-to_inst <- setdiff(pkgs, rownames(installed.packages()))
-if (length(to_inst)) {
-  try(install.packages(to_inst))
-}
-for (i in gh_pkgs) {
-  try(remotes::install_github(i, upgrade="never", force=FALSE))
-}
+    cat("\nNo packages installed.\n\n")
 
-still_missing <- setdiff(c(pkgs, names(gh_pkgs)), rownames(installed.packages()))
-if (length(still_missing)) {
-  cat("\nThe following packages could not be installed:\n",
-    paste("\t-", still_missing, collapse="\n"), "\n")
-} else {
-  cat("\nYou are all set! See you at the workshop.\n")
+  } else {
+
+    cat("\nHang tight ...\n\n* Checking R version\n\n")
+    if (getRversion() < "4.0") {
+      stop(paste0("R version ", getRversion(), " detected.",
+        " Please upgrade to R >= 4.0"))
+    } else {
+      cat("  - R version", as.character(getRversion()), "detected ... OK\n")
+    }
+
+    cat("\n* Installing R packages from CRAN\n\n")
+    pkgs <- c("bookdown", "remotes", "dismo",
+      "Distance", "forecast", "glmnet", "gbm", "intrval",
+      "knitr", "lme4", "maptools", "mefa4", "corrplot", "sf",
+      "mgcv", "MuMIn", "opticut", "partykit", "pscl", "raster",
+      "ResourceSelection", "shiny", "unmarked", "visreg")
+    to_inst <- setdiff(pkgs, rownames(installed.packages()))
+    if (length(to_inst) > 0L) {
+      if (dryrun) {
+        for (i in to_inst)
+          cat("  - Installing R package ", i, " ... OK\n")
+      } else {
+        try(install.packages(to_inst))
+      }
+    }
+
+    cat("\n* Installing R packages from GitHub\n\n")
+    gh_pkgs <- c(bSims = "psolymos/bSims", # use dev
+      detect = "psolymos/detect", # use dev
+      lhreg = "psolymos/lhreg") # not on CRAN
+    for (i in gh_pkgs) {
+      if (dryrun) {
+        cat("  - Installing R package ", i, " ... OK\n")
+      } else {
+        try(remotes::install_github(i, upgrade="never", force=FALSE))
+      }
+    }
+
+    still_missing <- setdiff(c(pkgs, names(gh_pkgs)), rownames(installed.packages()))
+    if (length(still_missing)) {
+      cat("\nThe following packages could not be installed:\n",
+        paste("\t-", still_missing, collapse="\n"), "\n")
+    } else {
+      cat("\nYou are all set! See you at the workshop.\n")
+    }
+
+  }
 }
