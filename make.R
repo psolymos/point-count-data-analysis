fb1d8524edfff4efaa3542454a5d65515604620f
Peter Solymos <psolymos@gmail.com>
2025-11-12 22:57:53 -0700

    Update day 1
    Signed-off-by: Peter Solymos <psolymos@gmail.com>

--- make.R
+++ make.R
@@ -1,8 +1,23 @@
 # Rscript --vanilla make.R
+# Rscript --vanilla make.R --day 1
+
+CONFIG <- rconfig::rconfig()
+str(CONFIG)
+
+DAY <- rconfig::value(CONFIG$day, "0")
+
+dir <- switch(
+    as.character(DAY[1]),
+    "0" = ".",
+    "1" = "day-01",
+    "2" = "day-02",
+    "3" = "day-03",
+    stop("Invalid day argument")
+)
+fl <- list.files(dir, recursive = TRUE)
+fl <- fl[grep("\\.qmd$", fl)]
 
 # Render qmd files
-fl <- list.files(".", recursive = TRUE)
-fl <- fl[grep("\\.qmd$", fl)]
 for (i in fl) {
-    quarto::quarto_render(i)
+    try(quarto::quarto_render(file.path(dir, i), output_format = "all"))
 }
